147ff59666308eab15e6877d0a27cc514c48d1c1
Zach Leslie <xaque208@gmail.com>
2017-08-12 18:44:34 -0700

    Update from svn

--- sysutils/facter/files/patch-lib_src_facts_freebsd_collection.cc
+++ sysutils/facter/files/patch-lib_src_facts_freebsd_collection.cc
@@ -1,11 +1,11 @@
---- lib/src/facts/freebsd/collection.cc.orig	2017-06-27 17:42:22 UTC
+--- lib/src/facts/freebsd/collection.cc.orig	2017-07-18 18:49:09 UTC
 +++ lib/src/facts/freebsd/collection.cc
 @@ -8,8 +8,12 @@
  #include <internal/facts/posix/kernel_resolver.hpp>
  #include <internal/facts/posix/ssh_resolver.hpp>
  #include <internal/facts/posix/timezone_resolver.hpp>
 -#include <internal/facts/resolvers/operating_system_resolver.hpp>
-+#include <internal/facts/posix/operating_system_resolver.hpp>
++#include <internal/facts/freebsd/operating_system_resolver.hpp>
  #include <internal/facts/freebsd/networking_resolver.hpp>
 +#include <internal/facts/freebsd/virtualization_resolver.hpp>
 +#include <internal/facts/freebsd/memory_resolver.hpp>
@@ -19,7 +19,7 @@
      {
          add(make_shared<posix::kernel_resolver>());
 -        add(make_shared<resolvers::operating_system_resolver>());
-+        add(make_shared<posix::operating_system_resolver>());
++        add(make_shared<freebsd::operating_system_resolver>());
          add(make_shared<freebsd::networking_resolver>());
          add(make_shared<bsd::uptime_resolver>());
          add(make_shared<bsd::filesystem_resolver>());
