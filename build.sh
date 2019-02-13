0f60bae6dda2dd371980aaa56625d21a0b4f370d
Zach Leslie <xaque208@gmail.com>
2019-02-12 21:26:57 -0800

    Includ ebuild file for current jail set

---
+++ build.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+sudo -i bash -c portshaker -v
+
+tree=default
+
+for build in larch12 larch11; do
+    sudo poudriere bulk -f /usr/local/etc/poudriere.d/$build.list -p $tree -j $build
+done
