e9c994e3054faa4884aa279af831d97b72f9b10c
Zach Leslie <xaque208@gmail.com>
2019-12-30 09:59:03 -0800

    No more larch11

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ sudo -i bash -c portshaker -v
 
 tree=default
 
-for build in larch12 larch11; do
+for build in larch12; do
     sudo poudriere bulk -f /usr/local/etc/poudriere.d/$build.list -p $tree -j $build
 done
