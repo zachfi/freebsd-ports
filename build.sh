f6c1bacd79d2cabed05a1ddfb0249b189d813322
Zach Leslie <xaque208@gmail.com>
2021-04-26 21:45:08 -0700

    Fix variable name

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ tree=default
 jails=(larch13 larch12)
 
 for jail in "${jails[@]}"; do
-    sudo poudriere bulk -f /usr/local/etc/poudriere.d/$build.list -p $tree -j $jail
+    sudo poudriere bulk -f /usr/local/etc/poudriere.d/$jail.list -p $tree -j $jail
 done
