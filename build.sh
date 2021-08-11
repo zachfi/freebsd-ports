2d6982addd8004c1076aa0d8352e03d4d099a040
Zach Leslie <xaque208@gmail.com>
2021-08-10 19:16:35 -0600

    Build only larch13

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 sudo -i bash -c portshaker -v
 
 tree=default
-jails=(larch13 larch12)
+jails=(larch13)
 
 for jail in "${jails[@]}"; do
     sudo poudriere bulk -f /usr/local/etc/poudriere.d/$jail.list -p $tree -j $jail
