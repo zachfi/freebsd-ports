859267e685684579e8a7476124a3d08f38f6eead
Zach Leslie <xaque208@gmail.com>
2022-09-12 16:06:52 +0000

    Update jails

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 sudo -i bash -c portshaker -v
 
 tree=default
-jails=(larch13)
+jails=(znet13-1)
 
 for jail in "${jails[@]}"; do
     sudo poudriere bulk -f /usr/local/etc/poudriere.d/$jail.list -p $tree -j $jail -J 2
