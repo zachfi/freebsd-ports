2fc7e69c24d21828e88f13645cd119bfd388d612
Zach Leslie <xaque208@gmail.com>
2022-11-24 02:49:46 +0000

    Build local list

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ tree=default
 jails=(znet13-1)
 
 for jail in "${jails[@]}"; do
-    sudo poudriere bulk -f /usr/local/etc/poudriere.d/$jail.list -p $tree -j $jail -J 2
+    # sudo poudriere bulk -f /usr/local/etc/poudriere.d/$jail.list -p $tree -j $jail -J 2
+    sudo poudriere bulk -f d/$jail.list -p $tree -j $jail -J 2
 done
