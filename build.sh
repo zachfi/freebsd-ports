94c9dc59f791a5f371cfd4e6ca2fa851de75261a
Zach Leslie <xaque208@gmail.com>
2023-04-21 15:35:18 +0000

    Updates

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-sudo -i bash -c portshaker -v
+#sudo -i bash -c portshaker -v
 
 tree=default
 jails=(pkg13-2)
 
 for jail in "${jails[@]}"; do
     # sudo poudriere bulk -f /usr/local/etc/poudriere.d/$jail.list -p $tree -j $jail -J 2
-    sudo poudriere bulk -f d/$jail.list -p $tree -j $jail -J 2
+    sudo poudriere bulk -f d/$jail.list -p $tree -j $jail -J 2 $@
+    #poudriere options -f d/$jail.list -p $tree -j $jail $@
 done
